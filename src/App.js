c83175260291913d2870b9fe60a3bbe9cf8a5354
nguyentaithao <thaogreenhouses@gmail.com>
2022-09-26 16:31:20 +0700

    add travis

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Leasdarn React ahihihaasdfasd thao
         </a>
       </header>
     </div>
